6a8ad17d434fe18e00c2645beca6a62edc10decf
Keivan Beigi <me@keivan.io>
2022-09-19 09:23:52 -0700

    Airbnb Based Config

--- index.js
+++ index.js
@@ -1,159 +1,178 @@
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: 'module',
-		ecmaFeatures: { modules: true }
-	},
-	plugins: ['import', 'autofix', 'json', 'simple-import-sort', 'filenames'],
-	extends: [
-		'eslint:recommended',
-		'plugin:import/recommended',
-	],
-	settings: {
-		'import/parsers': {
-		  '@typescript-eslint/parser': ['.ts', '.tsx']
-		},
-		'import/resolver': {
-		  typescript: {
-			alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-			project: 'tsconfig.json'
-		  }
-		}
-	  },
-	env: {
-		es6: true,
-		mocha: true
-	},
-	overrides: [
-		{
-			files: ['*.ts', '*.tsx'],
-			extends: [
-				'.ts.eslintrc.js'
-			]
-		},
-		{
-			files: ['*.json'],
-			extends: [
-				'plugin:json/recommended-with-comments'
-			],
-			rules: { quotes: ['error', 'double', { avoidEscape: true }] }
-		}
-	],
-	reportUnusedDisableDirectives: true,
-	rules: {
-		'filenames/match-exported': 'off',
-
-		// https://eslint.org/docs/rules/#possible-errors
-		'no-await-in-loop': 'off',
-		'no-extra-parens': ['warn', 'all', { nestedBinaryExpressions: false }],
-		'no-import-assign': 'error',
-		'no-template-curly-in-string': 'error',
-		'require-atomic-updates': 'off',
-
-		// https://eslint.org/docs/rules/#best-practices
-		'array-callback-return': ['error', { allowImplicit: true }],
-		'block-scoped-var': 'error',
-		'curly': ['error', 'multi-line'],
-		'default-case': 'warn',
-		'default-param-last': 'warn',
-		'dot-notation': 'warn',
-		'eqeqeq': 'error',
-		'no-alert': 'error',
-		'no-else-return': 'warn',
-		'no-empty-function': 'warn',
-		'no-eval': 'error',
-		'no-extend-native': 'error',
-		'no-fallthrough': 'error',
-		'no-floating-decimal': 'warn',
-		'no-multi-spaces': 'warn',
-		'no-redeclare': 'error',
-		'no-return-assign': 'error',
-		'no-return-await': 'off',
-		'no-self-assign': 'error',
-		'no-self-compare': 'error',
-		'require-await': 'error',
-
-		// https://eslint.org/docs/rules/#variables
-		'no-sequences': 'error',
-		'no-shadow': 'off',
-		'no-throw-literal': 'error',
-		'no-unused-vars': 'warn',
-		'no-use-before-define': 'off',
-
-		// https://eslint.org/docs/rules/#nodejs-and-commonjs
-		'handle-callback-err': 'warn',
-
-		// https://eslint.org/docs/rules/#stylistic-issues
-
-		'array-bracket-spacing': ['warn', 'never'],
-		'array-element-newline': ['warn', 'consistent'],
-		'arrow-spacing': ['warn', { before: true, after: true }],
-		'block-spacing': 'warn',
-		'brace-style': ['warn', '1tbs'],
-		'comma-dangle': ['warn', 'never'],
-		'comma-spacing': ['warn', { before: false, after: true }],
-		'computed-property-spacing': ['warn', 'never'],
-		'eol-last': ['warn', 'always'],
-		'func-call-spacing': ['warn', 'never'],
-		'func-style': ['warn', 'declaration'],
-		'implicit-arrow-linebreak': ['warn', 'beside'],
-		'indent': 'off',
-		'key-spacing': ['warn', { beforeColon: false }],
-		'keyword-spacing': ['warn', { before: true }],
-		'linebreak-style': ['off', 'unix'],
-		'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-		'max-depth': ['warn', { max: 4 }],
-		'max-len': ['warn', 240],
-		'max-statements-per-line': ['warn', { max: 1 }],
-		'multiline-comment-style': ['warn', 'separate-lines'],
-		'no-bitwise': ['error'],
-		'no-lonely-if': ['warn'],
-		'no-mixed-operators': ['warn'],
-		'no-multi-assign': ['warn'],
-		'no-multiple-empty-lines': ['warn', {
-			max: 1, maxEOF: 1, maxBOF: 0
-		}],
-		'no-trailing-spaces': ['warn', { skipBlankLines: true }],
-		'no-unneeded-ternary': ['warn'],
-		'no-whitespace-before-property': ['warn'],
-		// 'array-element-newline': ['warn', { multiline: true, minItems: 4 }],
-		'object-curly-newline': ['warn', {
-			ObjectExpression: {
-				multiline: true, consistent: true, minProperties: 3
-			}
-		}],
-		'object-property-newline': ['warn', { allowAllPropertiesOnSameLine: true }],
-		'object-curly-spacing': ['warn', 'always'],
-		'operator-linebreak': ['warn', 'after'],
-		'padded-blocks': ['warn', 'never'],
-		'quote-props': ['warn', 'consistent-as-needed'],
-		'quotes': ['error', 'single', { avoidEscape: true }],
-		'semi': ['error', 'always'],
-		'semi-style': ['error', 'last'],
-		'space-in-parens': ['warn', 'never'],
-		'space-infix-ops': 'warn',
-		'spaced-comment': ['warn', 'always', { markers: ['/'] }],
-		'dot-location': ['error', 'property'],
-		'function-call-argument-newline': ['error', 'consistent'],
-		// "function-paren-newline": ["error", "multiline"],
-		'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-		// "space-before-function-paren":["warn","never"],`
-
-		// https://eslint.org/docs/rules/#ecmascript-6
-		'no-restricted-imports': ['error', { patterns: ['*/motto.*/*'] }],
-
-		'import/first': 'error',
-		'import/extensions': ['off', 'always', { js: 'never', ts: 'never' }],
-		'import/namespace': ['off'],
-		'sort-imports': [
-			'warn',
-			{
-				ignoreCase: true,
-				ignoreDeclarationSort: true,
-				ignoreMemberSort: true,
-				memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple']
-			}
-		]
-	}
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:eslint-comments/recommended',
+  ],
+  env: {
+    jest: true,
+  },
+  plugins: [
+    'filenames',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  ignorePatterns: ['dist', 'build'],
+  rules: {
+    '@typescript-eslint/array-type': [
+      'warn',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'warn',
+    ],
+    '@typescript-eslint/no-extra-parens': 'warn',
+    '@typescript-eslint/no-floating-promises': [
+      'error',
+    ],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+      },
+    ],
+    '@typescript-eslint/return-await': [
+      'error',
+      'always',
+    ],
+    'class-methods-use-this': [
+      'off',
+    ],
+    'consistent-return': [
+      'off',
+    ],
+    'default-case': 'warn',
+    'eslint-comments/disable-enable-pair': [
+      'error',
+      {
+        allowWholeFile: true,
+      },
+    ],
+    'filenames/match-exported': 'warn',
+    'id-length': [
+      'warn',
+      {
+        max: 45,
+        min: 1,
+      },
+    ],
+    'import/no-cycle': 'warn',
+    'import/no-mutable-exports': 'warn',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'warn',
+      {
+        'alphabetize': {
+          caseInsensitive: true,
+          order: 'asc',
+        },
+        'groups': [
+          'builtin',
+          'external',
+          [
+            'internal',
+            'sibling',
+            'parent',
+          ],
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'max-classes-per-file': 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 180,
+        ignoreUrls: true,
+      },
+    ],
+    'max-nested-callbacks': [
+      'warn',
+      {
+        max: 4,
+      },
+    ],
+    'newline-per-chained-call': [
+      'warn',
+      {
+        ignoreChainWithDepth: 3,
+      },
+    ],
+    'no-await-in-loop': 'warn',
+    'no-console': 'off',
+    'no-continue': 'off',
+    'no-extra-parens': 'off',
+    'no-import-assign': 'error',
+    'no-multi-str': 'off',
+    'no-nested-ternary': 'warn',
+    'no-param-reassign': [
+      'warn',
+      {
+        props: false,
+      },
+    ],
+    'no-restricted-syntax': [
+      'off',
+    ],
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': 'off',
+    'no-void': [
+      'off',
+      {
+        allowAsStatement: true,
+      },
+    ],
+    'node/no-unsupported-features/es-syntax': 'off',
+    'quote-props': [
+      'warn',
+      'consistent-as-needed',
+    ],
+    'radix': [
+      'error',
+      'as-needed',
+    ],
+    'sort-imports': [
+      'warn',
+      {
+        allowSeparatedGroups: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'space-infix-ops': 'warn',
+  },
 };
-  
